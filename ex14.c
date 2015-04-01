b2ff0ba4b94d815eae3f49e5bdf865e4ba037a65
Sam <sam_hake@live.co.uk>
2015-04-01 21:27:43 +0100

    Add exersises 13-14.

---
+++ ex14.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+
+//forward declarations.
+int canPrintIt(char ch);
+void printLetters(char arg[]);
+
+void printArguments(int argc, char *argv[]) {
+	
+	int i = 0;
+	
+	for(i = 0; i < argc; i++) {
+		printLetters(argv[i]);
+	}	
+}
+
+void printLetters(char arg[]) {
+	
+	int i = 0;
+	for(i = 0; arg[i] != '\0'; i++) {
+		
+		char ch = arg[i];
+		
+		if(canPrintIt(ch)) {
+			printf("'%c' == %d", ch, ch);
+		}
+	}
+	
+	printf("\n");	
+}
+		
+int canPrintIt(char ch) {
+	
+	return isalpha(ch) || isblank(ch);
+
+}
+
+int main(int argc, char *argv[]) {
+	
+	printArguments(argc, argv);
+	
+	return 0;
+	
+}
+	
